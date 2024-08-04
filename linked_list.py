3957ac44b74059e420cc7bcf57d327c91af18e61
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-04 20:52:30 +0200

    fix append_at_beginning function

--- linked_list.py
+++ linked_list.py
@@ -19,11 +19,9 @@ class linked_list:
 
     def append_at_beginning(self, data):
         new_node = Node(data)
-        if self.head == None:
-            self.head = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
+        new_node.next = self.head.next
+        self.head.next = new_node
+            
 
     def length(self):
         cur = self.head
@@ -60,14 +58,12 @@ def main():
     link.append(3)
     link.append(4)
     link.append(5)
-
+    link.append_at_beginning(0)
 
     # print(f"the {link.length()} elemrnets in the linked list are: ")
     # link.print_list()
 
     print(f"the element at index 2 is: {link.get(2)}")
-
-    link.append_at_beginning(0)
 
     link.print_list()
 
