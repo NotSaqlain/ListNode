d94f0ec4546386ba41661f139126a382113b00fd
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-08-04 20:18:44 +0200

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    
+class linked_list:
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+
+    def print_list(self):
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+            print(cur.data)
+
+    def get(self, index):
+        if index >= self.length():
+            print("Error: 'Get' index out of range")
+            return None
+        cur = self.head
+        
+
+def main():
+
+    # create a linked list
+    link = linked_list()
+    link.append(1)
+    link.append(2)
+    link.append(3)
+    link.append(4)
+    link.append(5)
+
+
+    print(f"the {link.length()} elemrnets in the linked list are: ")
+    link.print_list()
+
+
+if __name__ == "__main__":
+    main()
